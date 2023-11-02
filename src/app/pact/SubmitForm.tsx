a9f7a50f4ca5a9993d47ccd0001ca0f61e017c22
Andy Tang <andy@kadena.io>
2023-11-02 10:01:42 +0100

    Add code preview

---
+++ src/app/pact/SubmitForm.tsx
@@ -0,0 +1,89 @@
+import { Box, Button, Stack, Text } from "@kadena/react-ui";
+import { FC, useState } from "react";
+import { l1Client } from "../utils/client";
+import { uploadModuleTransaction } from "./pact.utils";
+import { PreviewFormValues } from "./PreviewForm";
+
+type PreviewFormProps = {
+  values: PreviewFormValues;
+  onCancel: () => void;
+};
+
+export const SubmitForm: FC<PreviewFormProps> = ({ values, onCancel }) => {
+  const [result, setResult] = useState<{
+    success: boolean;
+    message: string;
+  } | null>(null);
+
+  const onSendLocalResult = async () => {
+    const publicKey = values.publicKey;
+  };
+
+  console.log(values);
+  return (
+    <div>
+      <Stack
+        direction="row"
+        margin="$md"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Box marginRight="$2">pact module file</Box>
+        {values.file?.item(0)?.name}
+      </Stack>
+      <Stack
+        direction="row"
+        margin="$md"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Box marginRight="$2">module data json</Box>
+        <textarea
+          id="contractData"
+          style={{
+            width: "100%",
+            minHeight: "120px",
+            resize: "vertical",
+          }}
+          defaultValue={values.contractData}
+        />
+      </Stack>
+      <Stack
+        direction="row"
+        margin="$md"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Box marginRight="$2">Chain ID</Box>
+        {values.chainId}
+      </Stack>
+      <Stack
+        direction="row"
+        margin="$md"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Box marginRight="$2">Network ID</Box>
+        {values.networkdId}
+      </Stack>
+      <Stack
+        direction="row"
+        margin="$md"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Box marginRight="$2">Public Key</Box>
+        {values.publicKey}
+      </Stack>
+      <Stack direction="column" margin="$md" justifyContent="flex-start">
+        <Text as="code" variant="code">
+          {values.result}
+        </Text>
+      </Stack>
+      <Stack direction="row" margin="$md" justifyContent="flex-start">
+        <Button onClick={onSendLocalResult}>Upload</Button>
+        <Button onClick={onCancel}>Back</Button>
+      </Stack>
+    </div>
+  );
+};
