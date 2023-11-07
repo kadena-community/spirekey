bfdbf6886c0a2e183e4f0e7002ef2ab2ba561223
Andy Tang <andy@kadena.io>
2023-11-07 10:11:08 +0100

    Add a login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import {
+  Box,
+  Button,
+  ContentHeader,
+  SelectField,
+  Stack,
+  Text,
+  TrackerCard,
+} from "@kadena/react-ui";
+import { useEffect, useState } from "react";
+
+type LoginProps = {
+  searchParams: {
+    returnUrl: string;
+  };
+};
+
+type Account = {
+  cid: string;
+  name: string;
+  account: string;
+  balance: string;
+};
+
+export default function Login({ searchParams }: LoginProps) {
+  const [accounts, setAccounts] = useState<Account[]>([]);
+  const [account, setAccount] = useState<Account | null>(null);
+  useEffect(() => {
+    const accounts = localStorage.getItem("accounts");
+    if (!accounts) return;
+    const accs = JSON.parse(accounts);
+    setAccounts(accs);
+    setAccount(accs[0]);
+  }, []);
+  return (
+    <Stack direction="column" alignItems="center" marginY="$lg">
+      <Box>
+        <ContentHeader
+          description={`Which account do you want to use to identify on ${searchParams.returnUrl}?`}
+          heading="Login"
+          icon="Account"
+        />
+        <AccountSelector
+          accounts={accounts}
+          account={account}
+          returnUrl={searchParams.returnUrl}
+        />
+      </Box>
+    </Stack>
+  );
+}
+
+const AccountSelector = ({
+  accounts,
+  account,
+  returnUrl,
+}: {
+  accounts: Account[];
+  account: Account | null;
+  returnUrl: string;
+}) => {
+  if (!account) return <Text>No account found, please register first.</Text>;
+  return (
+    <>
+      <SelectField
+        label="account"
+        selectProps={{ id: "account", ariaLabel: "Select your account" }}
+      >
+        {accounts.map((account) => (
+          <option value={account.account} key={account.cid}>
+            {account.name}
+          </option>
+        ))}
+      </SelectField>
+      <TrackerCard
+        icon="ManageKda"
+        labelValues={[
+          {
+            label: "Account",
+            value: account.account,
+            isAccount: true,
+          },
+          {
+            label: "Display Name",
+            value: account.name,
+          },
+          {
+            label: "Balance",
+            value: account.balance,
+          },
+        ]}
+        helperText="This is the account you will use to login."
+      />
+      <Button
+        as="a"
+        href={`${returnUrl}?cid=${account.cid}&name=${account.name}&account=${account.account}`}
+      >
+        Login
+      </Button>
+    </>
+  );
+};
