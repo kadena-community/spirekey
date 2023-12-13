2c6b9d76e0c5e650531c290233d20574cc097e2c
Andy Tang <andy@kadena.io>
2023-12-13 16:37:33 +0100

    Allow namespace to be set via env variables

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     CHAINWEB_URL: process.env.CHAINWEB_URL,
     WALLET_URL: process.env.WALLET_URL,
     STRING_SIG: "true",
-    NAMESPACE: "n_560eefcee4a090a24f12d7cf68cd48f11d8d2bd9",
+    NAMESPACE: process.env.NAMESPACE,
   },
 };
 
