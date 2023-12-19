aea2f3a794696c570869d4751a24fb77ae4f477e
Andy Tang <andy@kadena.io>
2023-12-19 09:35:59 +0100

    Add restore from network fields

--- src/app/restore/page.tsx
+++ src/app/restore/page.tsx
@@ -6,6 +6,7 @@ import {
   Button,
   Card,
   ContentHeader,
+  SelectField,
   Stack,
   TextField,
 } from "@kadena/react-ui";
@@ -13,6 +14,9 @@ import { useForm } from "react-hook-form";
 
 const FORM_DEFAULT = {
   caccount: "",
+  network: "fast-development",
+  fromNetwork: "fast-development",
+  host: "",
 };
 export type RestoreFormValues = typeof FORM_DEFAULT;
 
@@ -45,6 +49,25 @@ export default function RestorePage() {
       <Card fullWidth>
         <ContentHeader
           heading="WebAuthn Wallet"
+          description="On which network do you want to restore your account?"
+          icon="Account"
+        />
+        <SelectField
+          label="Network"
+          selectProps={{
+            id: "network",
+            ariaLabel: "network",
+            ...register("network"),
+          }}
+        >
+          <option value="mainnet01">mainnet01</option>
+          <option value="testnet04">testnet04</option>
+          <option value="fast-development">fast-development</option>
+        </SelectField>
+      </Card>
+      <Card fullWidth>
+        <ContentHeader
+          heading="From selected network"
           description="Restore an account using c:account"
           icon="Account"
         />
@@ -58,6 +81,39 @@ export default function RestorePage() {
           }
         />
         <Button onClick={onRestoreAccount}>Restore</Button>
+      </Card>
+      <Card fullWidth>
+        <ContentHeader
+          heading="From another network"
+          description="Restore an account from another network"
+          icon="Account"
+        />
+        <TextField
+          label="account"
+          inputProps={{ id: "caccount", ...register("caccount") }}
+          status={formState.errors.caccount ? "negative" : undefined}
+          helperText={
+            formState.errors.caccount?.message ||
+            "Enter the account name you want to restore, this should look like c:account"
+          }
+        />
+        <SelectField
+          label="Network"
+          selectProps={{
+            id: "network",
+            ariaLabel: "network",
+            ...register("network"),
+          }}
+        >
+          <option value="mainnet01">mainnet01</option>
+          <option value="testnet04">testnet04</option>
+          <option value="fast-development">fast-development</option>
+        </SelectField>
+        <TextField
+          label="host"
+          inputProps={{ id: "host", ...register("host") }}
+          helperText="Leave the host empty to use the default host"
+        />
       </Card>
     </Stack>
   );
