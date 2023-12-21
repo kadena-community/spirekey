421b4dbd2f31c5120e84a580f1a937940991a3b4
Andy Tang <andy@kadena.io>
2023-12-21 11:27:42 +0100

    Transfer page with some status notifications

--- src/app/transfer/page.tsx
+++ src/app/transfer/page.tsx
@@ -12,6 +12,7 @@ import {
   ContentHeader,
   Grid,
   GridItem,
+  Notification,
   Stack,
   TextField,
 } from "@kadena/react-ui";
@@ -53,7 +54,7 @@ export default function Page({ searchParams }: Props) {
     });
     sign(tx, activeDevice, "/transfer");
   };
-  const { doSubmit, result, status } = useSubmit(searchParams);
+  const { doSubmit, status } = useSubmit(searchParams);
   useEffect(() => {
     if (status === SubmitStatus.SUBMITABLE) doSubmit();
   }, [status, doSubmit]);
@@ -67,6 +68,16 @@ export default function Page({ searchParams }: Props) {
             description="Send KDA to another account"
             icon="BadgeAccount"
           />
+          {status === SubmitStatus.LOADING && (
+            <Notification role="status" color="info" hasCloseButton>
+              Transfer in progress
+            </Notification>
+          )}
+          {status === SubmitStatus.SUCCESS && (
+            <Notification role="status" color="positive" hasCloseButton>
+              Transfer successful
+            </Notification>
+          )}
           <Grid
             columns={{
               xs: 3,
