a8ad9549445ff64e6a9f872593e036102dd92057
Andy Tang <andy@kadena.io>
2024-01-11 14:31:07 +0100

    show Stacked cards

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -3,6 +3,10 @@
 import { useNetwork } from '@/context/NetworkContext';
 import { useAccounts } from '@/hooks/useProfiles';
 import { Box, Heading, MaskedValue, Stack, Text } from '@kadena/react-ui';
+import { sprinkles } from '@kadena/react-ui/theme';
+import classNames from 'classnames';
+
+import './cards.css';
 
 export default function Cards() {
   const { accounts } = useAccounts();
@@ -15,26 +19,43 @@ export default function Cards() {
           Cards
         </Heading>
       </Box>
-      {accounts.map((account) => (
-        <Box
-          borderStyle="solid"
-          borderWidth="hairline"
-          borderRadius="md"
-          borderColor="brand.primary.default"
-          padding="md"
-          style={{
-            width: '20rem',
-          }}
-        >
-          <Heading variant="h5" as="h2">
-            {account.devices.map((d) => d.identifier).join(', ')}
-          </Heading>
+      <Stack className="cards" flexDirection="column">
+        {accounts.map((account) => (
+          <Box
+            key={account.accountName + account.network}
+            className={classNames(
+              sprinkles({
+                position: 'relative',
+              }),
+              'card',
+            )}
+            style={{
+              width: '20rem',
+            }}
+          >
+            <Box
+              borderStyle="solid"
+              borderWidth="hairline"
+              borderRadius="md"
+              borderColor="brand.primary.default"
+              padding="md"
+              className={sprinkles({
+                backgroundColor: '$primarySurface',
+                position: 'absolute',
+                width: '100%',
+              })}
+            >
+              <Heading variant="h5" as="h2">
+                {account.devices.map((d) => d.identifier).join(', ')}
+              </Heading>
 
-          <MaskedValue value={account.accountName} />
+              <MaskedValue value={account.accountName} />
 
-          <Text>{network}</Text>
-        </Box>
-      ))}
+              <Text>{network}</Text>
+            </Box>
+          </Box>
+        ))}
+      </Stack>
     </Stack>
   );
 }
