deccbffa19ecfd36f2bac988f97f09ce8c5db753
Jesse van Muijden <jessevanmuijden@gmail.com>
2024-01-22 04:17:28 -0400

    feat(card): new card styling

---
+++ src/app/register/[step]/page.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { Box, Button, Stack } from '@kadena/react-ui';
+import { useParams, useRouter } from 'next/navigation';
+import { FormProvider, useForm } from 'react-hook-form';
+import { Color } from './steps/Color';
+import { Fingerprint } from './steps/Fingerprint';
+import { Icon } from './steps/Icon';
+import Card2 from '@/components/Card2/Card2';
+
+const STEPS = ['fingerprint', 'icon', 'color'];
+const FORM_DEFAULT = {
+  caccount: null,
+  icon: null,
+  color: null,
+};
+
+export default function Account() {
+  const { push } = useRouter();
+  const params = useParams();
+  const methods = useForm({ defaultValues: FORM_DEFAULT });
+
+  const currentStep = STEPS.indexOf(params.step as string);
+  const prevStep = STEPS[currentStep - 1];
+  const nextStep = STEPS[currentStep + 1];
+
+  const goToNextStep = () => {
+    if (!nextStep) return;
+    push(`/register/${nextStep}`);
+  };
+
+  const goToPrevStep = () => {
+    if (!prevStep) return;
+    push(`/register/${prevStep}`);
+  };
+
+  return (
+    <>
+      <Stack flexDirection={'column'} gap={'md'} marginInline={'md'}>
+        <Box width={'100%'}>
+          <Card2
+            account={{
+              accountName: '',
+              balance: '',
+              devices: [],
+              network: '',
+            }}
+            onClick={() => {}}
+            isActive={true}
+            isCollapsed={false}
+          />
+        </Box>
+        <FormProvider {...methods}>
+          {params.step === 'fingerprint' && <Fingerprint />}
+          {params.step === 'icon' && <Icon />}
+          {params.step === 'color' && <Color />}
+        </FormProvider>
+        {prevStep && <Button onClick={goToPrevStep}>Back</Button>}
+        {nextStep && <Button onClick={goToNextStep}>Next</Button>}
+      </Stack>
+    </>
+  );
+}
---
+++ src/components/Card2/Card2.css.ts
@@ -0,0 +1,184 @@
+import { atoms } from '@kadena/react-ui/theme';
+import { style } from '@vanilla-extract/css';
+import { KodeMono } from '@kadena/fonts';
+
+export const card = style([
+  atoms({
+    position: 'relative',
+    overflow: 'hidden',
+  }),
+  {
+    maxWidth: '600px',
+    color: 'black',
+    margin: '0 auto',
+    aspectRatio: '8560 / 5398',
+    height: 'auto',
+    flexShrink: 0,
+    selectors: {
+      '&:before': {
+        content: '',
+        position: 'absolute',
+        inset: 0,
+        borderRadius: 'inherit',
+        padding: '1px',
+        border: '1px solid transparent',
+        background:
+          'linear-gradient(0deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.25)) border-box',
+        mask: 'linear-gradient(#f00 0 0) padding-box, linear-gradient(#f00 0 0)',
+        maskComposite: 'xor, exclude',
+      },
+    },
+  },
+]);
+
+export const cardBackground = style([
+  atoms({
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  }),
+  {
+    borderRadius: '1rem',
+    objectFit: 'cover',
+    objectPosition: 'center',
+    zIndex: -1,
+  },
+]);
+
+export const cardContentContainer = style([
+  atoms({
+    height: '100%',
+  }),
+  {
+    paddingBlockStart: '1.5rem',
+    paddingBlockEnd: '2rem',
+    paddingInline: '2.5rem',
+  },
+]);
+
+export const cardLogo = style([
+  atoms({
+  }),
+  {
+    height: '2rem',
+    width: 'auto',
+    objectFit: 'contain',
+    objectPosition: 'bottom',
+  },
+]);
+
+export const accountAlias = style([
+  atoms({
+    whiteSpace: 'nowrap',
+  }),
+  {
+    color: 'black',
+    textOverflow: 'ellipsis',
+  },
+]);
+
+export const accountAliasContainer = style([
+  atoms({
+    flexGrow: 0,
+  }),
+]);
+
+export const account = style([
+  atoms({
+  }),
+  {
+    color: 'black',
+  },
+]);
+
+export const accountIcon = style([
+  atoms({
+    marginInline: 'md',
+  }),
+  {
+    color: 'black',
+  },
+]);
+
+export const accountIconInner = style([
+  atoms({
+    height: '100%',
+    display: 'block',
+  }),
+  {
+    color: 'black',
+  },
+]);
+
+export const devicesContainer = style([
+  atoms({
+    flexGrow: 1,
+  }),
+]);
+
+export const device = style([
+  atoms({
+    marginInlineStart: 'md',
+  }),
+]);
+
+export const cardContentCenter = style([
+  atoms({
+    marginBlockStart: 'lg',
+  }),
+]);
+
+export const network = style([
+  atoms({
+    fontWeight: 'bodyFont.regular',
+    fontSize: 'lg', // is not applied for some reason
+  }),
+  {
+    fontSize: 'large', // while this is applied
+  }
+]);
+
+export const cardContentBottom = style([
+  atoms({
+  }),
+  {
+    color: 'black',
+  },
+]);
+
+export const balanceLabel = style([
+  atoms({
+    fontSize: 'md',
+  }),
+]);
+
+export const balance = style([
+  atoms({
+    fontFamily: 'codeFont',
+    fontWeight: 'monoFont.bold',
+    fontSize: 'lg',
+    marginInlineStart: 'md',
+  }),
+  {
+    marginBlockEnd: '-1.25px',
+  },
+]);
+
+
+export const transactionsLabel = style([
+  atoms({
+    fontSize: 'md',
+  }),
+]);
+
+export const transactions = style([
+  atoms({
+    fontFamily: 'codeFont',
+    fontWeight: 'monoFont.bold',
+    fontSize: 'lg',
+    marginInlineStart: 'md',
+  }),
+  {
+    marginBlockEnd: '-1.25px',
+  },
+]);
