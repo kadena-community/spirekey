514c3763c3cad75f3be54bb2614a66d47969bfac
Jesse van Muijden <jessevanmuijden@gmail.com>
2024-01-23 08:56:19 -0400

    feat(routes): add receive and transaction routes + breadcrumbs

--- src/app/accounts/[caccount]/devices/[cid]/receive/page.tsx
+++ src/app/accounts/[caccount]/devices/[cid]/receive/page.tsx
@@ -1,27 +1,23 @@
 'use client';
 
-// import { transfer } from '@/utils/transfer';
 import { Breadcrumbs, BreadcrumbsItem, Button, TextField } from '@kadena/react-ui';
-import { useParams, useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const FORM_DEFAULTS = {
-  to: '',
   amount: 0,
 };
 type FormValues = typeof FORM_DEFAULTS;
 
-export default function SendPage() {
+export default function ReceivePage() {
   const params = useParams();
-  const router = useRouter();
   const { getValues, handleSubmit, register } = useForm({
     defaultValues: FORM_DEFAULTS,
     reValidateMode: 'onBlur',
   });
   const onSubmit = async (data: FormValues) => {
     console.log(data);
-    router.push(`/accounts/${params.caccount}/devices/${params.cid}/transactions`);
   };
   return (
     <div>
@@ -33,19 +29,18 @@ export default function SendPage() {
           {decodeURIComponent(params.cid.toString())}
         </BreadcrumbsItem>
         <BreadcrumbsItem>
-          Send
+          Receive
         </BreadcrumbsItem>
       </Breadcrumbs>
-      <h1>Send</h1>
+      <h1>Receive</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField id="to" label="To" {...register('to')} />
         <TextField
           id="amount"
           type="number"
           label="Amount"
           {...register('amount')}
         />
-        <Button type="submit">Send</Button>
+        <Button type="submit">Receive</Button>
       </form>
     </div>
   );
--- src/app/accounts/[caccount]/devices/add/page.tsx
+++ src/app/accounts/[caccount]/devices/add/page.tsx
@@ -1,10 +1,21 @@
 'use client';
 
-import { Heading } from '@kadena/react-ui';
+import { Breadcrumbs, BreadcrumbsItem, Heading } from '@kadena/react-ui';
+import { useParams } from 'next/navigation';
 
 export default function AddDevice() {
+  const params = useParams();
+
   return (
     <>
+      <Breadcrumbs>
+        <BreadcrumbsItem href={`/accounts/${params.caccount}`}>
+          {decodeURIComponent(params.caccount.toString())}
+        </BreadcrumbsItem>
+        <BreadcrumbsItem>
+          Add device
+        </BreadcrumbsItem>
+      </Breadcrumbs>
       <Heading>Add Device</Heading>
     </>
   );
