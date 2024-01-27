bc138cdad3e20792ec144218483e04dd6c049e57
Jesse van Muijden <jessevanmuijden@gmail.com>
2024-01-27 09:42:01 -0400

    feat(homepage): add new layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,209 +1,50 @@
 'use client';
 
-import { AddWallet } from '@/components/AddWallet';
-import { Loader } from '@/components/CreateWalletLoader/Loader';
-import { FundAccount } from '@/components/FundAccount';
-import { NetworkSelector } from '@/components/NetworkSelector';
-import { Restore } from '@/components/RestoreWallet';
-import { Scan } from '@/components/Scan';
-import { useNetwork } from '@/context/NetworkContext';
 import { useAccounts } from '@/hooks/useAccounts';
-import { registerAccount } from '@/utils/register';
-import { getNewWebauthnKey } from '@/utils/webauthnKey';
 import {
-  Button,
-  Card,
-  ContentHeader,
-  MaskedValue,
+  Heading,
+  Link,
   Stack,
-  Table,
-  Text,
-  TextField,
-  TrackerCard,
 } from '@kadena/react-ui';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import useSWR from 'swr';
-
-type Transaction = {
-  fromAccount: string;
-  toAccount: string;
-  amount: string;
-  blockTime: string;
-  requestKey: string;
-};
-
-const FORM_DEFAULT = {
-  displayName: '',
-};
-const Register = () => {
-  const { register, getValues } = useForm({
-    defaultValues: FORM_DEFAULT,
-    reValidateMode: 'onBlur',
-  });
-  const { storeAccount } = useAccounts();
-  const [isLoading, setIsLoading] = useState(false);
-  const [result, setResult] = useState('');
-  const onRegister = async () => {
-    const { displayName } = getValues();
-    if (!displayName) throw new Error('Display name is required');
-    setIsLoading(true);
-
-    const { credentialId, publicKey } = await getNewWebauthnKey(displayName);
-
-    const caccount = await registerAccount({
-      credentialPubkey: publicKey,
-      credentialId: credentialId,
-      displayName,
-      domain: window.location.hostname,
-    });
-    await storeAccount(caccount);
-    setIsLoading(false);
-    setResult(caccount);
-  };
-
-  if (result)
-    return (
-      <Card fullWidth>
-        <Stack flexDirection="column" gap="md" margin="md">
-          <ContentHeader
-            heading="WebAuthn Wallet"
-            description="Create an account using WebAuthn"
-            icon="Account"
-          />
-          <Text>Your account has been forged successfully!</Text>
-          <Text bold>{result}</Text>
-        </Stack>
-      </Card>
-    );
-
-  if (isLoading)
-    return (
-      <Card fullWidth>
-        <Stack flexDirection="column" gap="md" margin="md">
-          <Text>Your wallet is forging...</Text>
-          <Loader />
-        </Stack>
-      </Card>
-    );
-  return (
-    <Card fullWidth>
-      <Stack flexDirection="column" gap="md" margin="md">
-        <ContentHeader
-          heading="WebAuthn Wallet"
-          description="Create an account using WebAuthn"
-          icon="Account"
-        />
-        <TextField
-          label="Display Name"
-          {...{
-            id: 'display-name',
-            ...register('displayName', { required: true }),
-          }}
-          info="This name is only for your convienience to recognize your device."
-          description="This name will be stored on the blockchain, don't use any sensitive information."
-        />
-        <Button onClick={onRegister}>Register</Button>
-      </Stack>
-    </Card>
-  );
-};
+import Image from 'next/image';
+import logo from '@/assets/images/bennuKey.svg';
 
 export default function Home() {
-  const { activeAccount } = useAccounts();
-  const { chainwebDataUrl } = useNetwork();
-  const { data, error, isLoading } = useSWR(
-    `${chainwebDataUrl}/txs/account/${activeAccount?.account}`,
-    async (url: string) => {
-      if (!activeAccount) return [];
-      return await fetch(url).then((res) => res.json());
-    },
-  );
-
-  if (activeAccount) {
-    return (
-      <Stack margin="md" flexDirection="column" gap="md">
-        <NetworkSelector />
-        <Card fullWidth>
-          <Stack flexDirection="column" gap="md">
-            <ContentHeader
-              heading="WebAuthn Wallet"
-              description="Manage your WebAuthn account"
-              icon="Account"
-            />
-            <TrackerCard
-              icon="ManageKda"
-              labelValues={[
-                {
-                  label: 'Account',
-                  value: activeAccount.account,
-                  isAccount: true,
-                },
-                {
-                  label: 'Balance',
-                  value: `${activeAccount.balance} KDA`,
-                },
-              ]}
-            />
-            <Table.Root striped>
-              <Table.Head>
-                <Table.Tr>
-                  <Table.Th>From</Table.Th>
-                  <Table.Th>To</Table.Th>
-                  <Table.Th>KDA</Table.Th>
-                  <Table.Th>Date</Table.Th>
-                </Table.Tr>
-              </Table.Head>
-              <Table.Body>
-                {data?.map((tx: Transaction, index: number) => (
-                  <Table.Tr key={tx.requestKey + index}>
-                    <Table.Td>
-                      <MaskedValue value={tx.fromAccount} />
-                    </Table.Td>
-                    <Table.Td>
-                      <MaskedValue value={tx.toAccount} />
-                    </Table.Td>
-                    <Table.Td>
-                      <span
-                        style={{
-                          color:
-                            tx.fromAccount === activeAccount.account
-                              ? 'red'
-                              : 'green',
-                        }}
-                      >
-                        {tx.amount}
-                      </span>
-                    </Table.Td>
-                    <Table.Td>
-                      {new Date(tx.blockTime).toLocaleDateString()}
-                    </Table.Td>
-                  </Table.Tr>
-                ))}
-              </Table.Body>
-            </Table.Root>
-          </Stack>
-        </Card>
-        <Card fullWidth>
-          <Stack flexDirection="column" gap="md" margin="md">
-            <ContentHeader
-              heading="Fund account"
-              description="Add some funds to your account (Only works on Devnet or Testnet)"
-              icon="Account"
-            />
-            <FundAccount />
-          </Stack>
-        </Card>
-      </Stack>
-    );
-  }
+  const { accounts } = useAccounts();
 
   return (
-    <Stack flexDirection="column" gap="md" margin="md">
-      <Register />
-      <AddWallet />
-      <Restore />
-      <Scan />
+    <Stack
+      gap="md"
+      flexDirection="column"
+      justifyContent="space-around"
+      width="100%"
+      style={{ height: '100svh' }}
+    >
+      <Stack
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Image src={logo} alt="BennuKey logo" style={{ marginTop: '2rem' }} />
+        <Heading variant="h5" as="h2" style={{ lineHeight: 0.8 }}>
+          BennuKey
+        </Heading>
+        <Heading
+          variant="h3"
+          as="h1"
+          style={{ marginTop: 0, lineHeight: 0.8, marginBottom: '2rem' }}
+        >
+          Wallet
+        </Heading>
+      </Stack>
+      <Stack
+        flexDirection="row"
+        justifyContent="center"
+        gap="md"
+      >
+        {accounts.length > 0 && <Link href={'/accounts'}>Accounts</Link>}
+        <Link href={'/restore'}>Restore</Link>
+        <Link href={'/register'}>Register</Link>
+      </Stack>
     </Stack>
   );
 }
