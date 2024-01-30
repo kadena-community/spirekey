eaa8f3338d9e2f30092bc09c9ce3038c807550d8
Andy Tang <andy@kadena.io>
2024-01-30 10:55:23 +0100

    Hide icon if no alias is present yet

--- src/components/Card/Alias.tsx
+++ src/components/Card/Alias.tsx
@@ -1,17 +1,12 @@
 import { Box, Heading, SystemIcon } from '@kadena/react-ui';
-import {
-  accountAlias,
-  accountIcon,
-  accountIconInner,
-} from './Card.css';
+import { accountAlias, accountIcon, accountIconInner } from './Card.css';
 
 type CardProps = {
   title?: string;
 };
 
-export default function Alias({
-  title = '',
-}: CardProps) {
+export default function Alias({ title = '' }: CardProps) {
+  if (!title) return null;
   return (
     <>
       <Heading as="h3" variant="h4" className={accountAlias}>
