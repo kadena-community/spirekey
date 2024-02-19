b72ce0d71c43dfada8c85b721a624725765e22a4
Randy Daal <randy@randynamic.com>
2024-02-19 12:21:39 +0100

    fixed prices for pizza

--- src/app/v1/example/delivery/page.tsx
+++ src/app/v1/example/delivery/page.tsx
@@ -60,8 +60,8 @@ export default function DeliveryPage({ searchParams }: DeliveryProps) {
       merchantAccount: 'c:-BtZKCieonbuxQHJocDqdUXMZgHwN4XDNQjXXSaTJDo',
       merchantPublicKey:
         'WEBAUTHN-a5010203262001215820c4518d145cd1ca74d6371dfd24fec692d770ef13335e299533e0cf2bd11286a2225820b956dd1d7d48d3bb4e3a47c0a1cd70c7e3751f0e3fabf50c58ab22fc07033950',
-      deliveryPrice: 3.25,
-      orderPrice: getValues('amount') * price,
+      deliveryPrice: 2.55,
+      orderPrice: 2.55,
       orderId: id,
     });
     const orderId = createOrderId({
