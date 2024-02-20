8e3e0ca13c98d966354978fe3bf417754b3eb9d2
Ashwin van Dijk <contact@ashwinvandijk.nl>
2024-02-20 12:55:40 +0100

    Display the transactions properly

--- src/components/MaskedValue/MaskedValue.tsx
+++ src/components/MaskedValue/MaskedValue.tsx
@@ -25,11 +25,11 @@ export const MaskedValue: FC<IMaskedValueProps> = ({
   const end = value.slice(-endUnmaskedValues);
 
   return (
-    <div
+    <span
       data-testid="accountName"
       className={classNames(className, valueContainer)}
     >
-      {start}...{end}
-    </div>
+      {value.length <= 8 ? value : `${start}...${end}`}
+    </span>
   );
 };
