2cc3646a792091cd4fc2c632565e98bac219e4c0
Andy Tang <andy@kadena.io>
2024-02-28 20:55:12 +0100

    Add background to welcome page

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,28 +1,32 @@
 'use client';
 
 import logo from '@/assets/images/SpireKey-logo.svg';
+import { Background } from '@/components/Background/Background';
 import { ButtonLink } from '@/components/ButtonLink/ButtonLink';
 import { Stack } from '@kadena/react-ui';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <Stack
-      gap="md"
-      flexDirection="column"
-      justifyContent="space-around"
-      width="100%"
-      style={{ height: '100svh' }}
-    >
-      <Stack flexDirection="column" alignItems="center" gap="sm">
-        <Image src={logo} alt="SpireKey logo" style={{ marginTop: '2rem' }} />
+    <>
+      <Background />
+      <Stack
+        gap="md"
+        flexDirection="column"
+        justifyContent="space-around"
+        width="100%"
+        style={{ height: '100svh' }}
+      >
+        <Stack flexDirection="column" alignItems="center" gap="sm">
+          <Image src={logo} alt="SpireKey logo" style={{ marginTop: '2rem' }} />
+        </Stack>
+        <Stack flexDirection="row" justifyContent="center" gap="xl">
+          <ButtonLink href={'/recover'} variant="secondary">
+            Recover
+          </ButtonLink>
+          <ButtonLink href={'/register'}>Register</ButtonLink>
+        </Stack>
       </Stack>
-      <Stack flexDirection="row" justifyContent="center" gap="xl">
-        <ButtonLink href={'/recover'} variant="secondary">
-          Recover
-        </ButtonLink>
-        <ButtonLink href={'/register'}>Register</ButtonLink>
-      </Stack>
-    </Stack>
+    </>
   );
 }
