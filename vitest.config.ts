daf3047f8e7c91dc56efdc3e834ce4518e23b9c3
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-04-16 20:28:06 +0200

    feat: add coverage vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig, mergeConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   resolve: {
@@ -11,6 +11,17 @@ export default defineConfig({
     environment: 'jsdom',
     env: {
       NAMESPACE: 'n_eef68e581f767dd66c4d4c39ed922be944ede505',
+    },
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      thresholds: {
+        lines: 90,
+        functions: 90,
+        branches: 90,
+        statements: 90,
+        autoUpdate: false,
+      },
     },
   },
 });
