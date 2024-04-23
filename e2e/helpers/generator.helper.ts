c91b70280a2d8caf8a4792abba405d2c5021be01
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-04-23 16:56:01 +0200

    feat: add faker to generate aliases

--- e2e/helpers/generator.helper.ts
+++ e2e/helpers/generator.helper.ts
@@ -1,10 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 export async function generateAlias(): Promise<string> {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < 10; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters.charAt(randomIndex);
-  }
-  return result;
+  return faker.person.fullName();
 }
