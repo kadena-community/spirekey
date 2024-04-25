2b19eee02a81869002f56fba0343c71dfa8b73ce
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-04-25 11:10:55 +0200

    remove obsolete script, update vitest coverage

--- src/utils/deployScript.ts
+++
@@ -1,8 +0,0 @@
-import { deployFile } from './deploy';
-
-if (process.argv.length > 1) {
-  const [configFilePath, signersFilePath] = process.argv.slice(2);
-  if (!configFilePath) throw new Error('No config file path provided');
-  if (!signersFilePath) throw new Error('No signers file path provided');
-  deployFile(configFilePath, signersFilePath);
-}
