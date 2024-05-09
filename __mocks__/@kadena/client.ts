040078da11c391167f0dad960e17339eaa9560ff
Andy Tang <andy@kadena.io>
2024-05-09 11:01:18 +0200

    Add tests for fetching accounts from multiple chains

---
+++ __mocks__/@kadena/client.ts
@@ -0,0 +1,43 @@
+import type { ICommandResult, ITransactionDescriptor } from '@kadena/client';
+import { vi } from 'vitest';
+
+const response = {
+  reqKey: 'test-request-key',
+  gas: 700,
+  logs: 'abc',
+  continuation: null,
+  txId: 1,
+  metaData: {
+    blockHash: 'abc',
+    blockTime: 123,
+    blockHeight: 123,
+    prevBlockHash: 'abc',
+  },
+};
+
+export const successResponse: ICommandResult = {
+  ...response,
+  result: { status: 'success', data: {} },
+};
+
+export const errorResponse: ICommandResult = {
+  ...response,
+  result: { status: 'failure', error: {} },
+};
+
+const submitResponse: ITransactionDescriptor = {
+  requestKey: 'test-request-key',
+  chainId: '1',
+  networkId: 'development',
+};
+
+const l1Client = {
+  local: vi.fn().mockResolvedValue(successResponse),
+  listen: vi.fn().mockResolvedValue(successResponse),
+  pollOne: vi.fn().mockResolvedValue(successResponse),
+  submit: vi.fn().mockResolvedValue(submitResponse),
+};
+
+module.exports =
+  await vi.importActual<typeof import('@kadena/client')>('@kadena/client');
+export const createClient = () => l1Client;
