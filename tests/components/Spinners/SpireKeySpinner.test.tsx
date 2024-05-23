6b7f6f6e82f09953f55151156bb05b7cfe8ba22d
Jesse van Muijden <jesse@kadena.io>
2024-05-23 07:30:30 -0400

    feat(spinner): refactor test even more

--- tests/components/Spinners/SpireKeySpinner.test.tsx
+++ tests/components/Spinners/SpireKeySpinner.test.tsx
@@ -1,18 +1,10 @@
 import { SpireKeySpinner } from '@/components/Spinners/SpireKeySpinner';
+import { render } from '@testing-library/react';
 import React from 'react';
-import { afterEach, beforeAll, describe, expect, it } from 'vitest';
-import MatchMediaMock from 'vitest-matchmedia-mock';
-import { cleanup, render, screen } from '../setup';
+import { describe, expect, it } from 'vitest';
+import { screen } from '../setup';
 
 describe('SpireKeySpinner', () => {
-  let matchMediaMock = new MatchMediaMock();
-
-  beforeAll(() => {
-    matchMediaMock.useMediaQuery('(prefers-color-scheme: dark)');
-  });
-
-  afterEach(() => cleanup());
-
   describe('When showing the SpireKeySpinner component', () => {
     it('has seven light beams', () => {
       render(<SpireKeySpinner />);
