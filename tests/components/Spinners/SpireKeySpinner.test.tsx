b2e04fc741ae30a075b4c692fd48e265694f6792
Jesse van Muijden <jesse@kadena.io>
2024-05-23 07:07:36 -0400

    feat(spinner): refactor test

--- tests/components/Spinners/SpireKeySpinner.test.tsx
+++ tests/components/Spinners/SpireKeySpinner.test.tsx
@@ -6,16 +6,16 @@ import { cleanup, render, screen } from '../setup';
 
 describe('SpireKeySpinner', () => {
   let matchMediaMock = new MatchMediaMock();
+
   beforeAll(() => {
     matchMediaMock.useMediaQuery('(prefers-color-scheme: dark)');
   });
+
   afterEach(() => cleanup());
+
   describe('When showing the SpireKeySpinner component', () => {
-    const renderSpireKeySpinner = () => {
-      render(<SpireKeySpinner />);
-    };
     it('has seven light beams', () => {
-      renderSpireKeySpinner();
+      render(<SpireKeySpinner />);
       expect(screen.getByRole('status').childElementCount).toBe(7);
     });
   });
