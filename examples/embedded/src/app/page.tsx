59304216453f5499f4c05ae3e63a945883592ae3
Ashwin van Dijk <contact@ashwinvandijk.nl>
2024-05-30 16:26:56 +0200

    Add sign button

--- examples/embedded/src/app/page.tsx
+++ examples/embedded/src/app/page.tsx
@@ -7,34 +7,52 @@ import styles from './styles.module.css';
 
 export default function Home() {
   const [events, setEvents] = useState<SpireKeyEvent[]>([]);
+  const [account, setAccount] = useState();
 
   useEffect(() => {
     const { onEvent } = initSpireKey({ hostUrl: 'http://localhost:1337' });
 
     onEvent((event) => {
       setEvents((events) => [...events, event]);
+
+      if (event.name === 'account') {
+        setAccount(event.payload as any);
+      }
     });
   }, []);
 
+  const signTransaction = () => {
+    const transaction = '@TODO';
+    window.spireKey.sign(transaction);
+  };
+
   return (
     <main>
-      <button onClick={() => window.spireKey.connect()}>Connect</button>
+      {!account && (
+        <button onClick={() => window.spireKey.connect()}>Connect</button>
+      )}
+      {account && <button onClick={signTransaction}>Sign</button>}
 
-      {events.map((event, i) => (
-        <div key={i} className={styles.event}>
-          <div>
-            <strong>Name</strong>
-            <br /> {event.name}
-          </div>
+      <details>
+        <summary>View events</summary>
+        <div>
+          {events.map((event, i) => (
+            <div key={i} className={styles.event}>
+              <div>
+                <strong>Name</strong>
+                <br /> {event.name}
+              </div>
 
-          {event.payload && (
-            <div>
-              <strong>Payload</strong>
-              <pre>{JSON.stringify(event.payload, null, 2)}</pre>
+              {event.payload && (
+                <div>
+                  <strong>Payload</strong>
+                  <pre>{JSON.stringify(event.payload, null, 2)}</pre>
+                </div>
+              )}
             </div>
-          )}
+          ))}
         </div>
-      ))}
+      </details>
     </main>
   );
 }
