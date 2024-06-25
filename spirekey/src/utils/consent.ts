5bab626385450704d94f2d781cf4c2f216794030
Andy Tang <andy@kadena.io>
2024-06-25 16:52:42 +0200

    Find signees and candidates to port over for the tx

--- spirekey/src/utils/consent.ts
+++ spirekey/src/utils/consent.ts
@@ -1,5 +1,34 @@
 import { Account } from '@kadena-spirekey/types';
+import { IPactCommand } from '@kadena/client';
+import { IUnsignedCommand } from '@kadena/types';
 
-export const getDevicesFor = (accounts: Account[]) => {
-  return accounts;
+type Signees = {
+  accounts: Account[];
+  candidates: Account[];
 };
+
+export const getAccountsForTx =
+  (accounts: Account[]) => (tx: IUnsignedCommand) => {
+    const { meta, networkId, signers }: IPactCommand = JSON.parse(tx.cmd);
+    const { chainId } = meta;
+    const pubKeysInTx = signers.map((s) => s.pubKey);
+    return accounts.reduce(
+      (acc: Signees, curr) => {
+        if (
+          !curr.devices
+            .flatMap((d) => d.guard.keys)
+            .some((k) => pubKeysInTx.includes(k))
+        )
+          return acc;
+        if (curr.networkId !== networkId)
+          return { ...acc, candidates: [...acc.candidates, curr] };
+        if (!curr.chainIds.includes(chainId))
+          return { ...acc, candidates: [...acc.candidates, curr] };
+        return { ...acc, accounts: [...acc.accounts, curr] };
+      },
+      {
+        accounts: [],
+        candidates: [],
+      },
+    );
+  };
--- spirekey/tests/utils/consent.test.ts
+++ spirekey/tests/utils/consent.test.ts
@@ -1,17 +1,133 @@
-import { describe } from 'vitest';
+import { getAccountsForTx } from '@/utils/consent';
+import { Account } from '@kadena-spirekey/types';
+import { type ChainId, createTransactionBuilder } from '@kadena/client';
+import { describe, expect, it } from 'vitest';
+
+const createMockAccounts = ({
+  pubKeys,
+  chainId,
+  networkId,
+}: {
+  pubKeys: string[];
+  chainId: ChainId;
+  networkId: string;
+}): Account[] =>
+  pubKeys.map((pubKey, i) => ({
+    networkId,
+    accountName: `c:${pubKey}`,
+    chainIds: [chainId],
+    balance: '0.0',
+    alias: `SpireKey Account ${i}`,
+    minApprovals: 1,
+    minRegistrationApprovals: 1,
+    devices: [
+      {
+        guard: {
+          keys: [pubKey],
+          pred: 'keys-any',
+        },
+        color: '#000',
+        domain: 'https://spirekey.kadena.io',
+        deviceType: 'phone',
+        'credential-id': 'credid',
+      },
+    ],
+  }));
+
+const getTxFixture = ({
+  pubKeys,
+  networkId,
+  chainId,
+}: {
+  pubKeys: string[];
+  networkId: string;
+  chainId: ChainId;
+}) => {
+  const tx = createTransactionBuilder({
+    meta: { chainId },
+    networkId,
+  }).execution(`(+ 1 1)`);
+  for (const pubKey of pubKeys) {
+    tx.addSigner({ pubKey, scheme: 'WebAuthn' });
+  }
+  return tx.createTransaction();
+};
 
 describe('consent', () => {
   describe('when getting accounts to sign with', () => {
     describe('and there are no applicable accounts', () => {
-      describe('but the pubkey exists on a different network', () => {});
-      describe('but the pubkey exists on a different chain', () => {});
-      describe('and the pubkey does not exists on a different network', () => {});
+      describe('but the pubkey exists on a different network', () => {
+        it('should add the account to the candidates list', () => {
+          const pubKeys = ['WEBAUTHN-alice'];
+          const tx = getTxFixture({
+            pubKeys,
+            chainId: '4',
+            networkId: 'testnet04',
+          });
+          const accounts = createMockAccounts({
+            chainId: '4',
+            networkId: 'mainnet01',
+            pubKeys,
+          });
+          const devices = getAccountsForTx(accounts)(tx);
+          expect(devices.accounts).toEqual([]);
+          expect(devices.candidates).toEqual(accounts);
+        });
+      });
+      describe('but the pubkey exists on a different chain', () => {
+        it('should add the account to the candidates list', () => {
+          const pubKeys = ['WEBAUTHN-alice'];
+          const tx = getTxFixture({
+            pubKeys,
+            chainId: '4',
+            networkId: 'testnet04',
+          });
+          const accounts = createMockAccounts({
+            chainId: '18',
+            networkId: 'testnet04',
+            pubKeys,
+          });
+          const devices = getAccountsForTx(accounts)(tx);
+          expect(devices.accounts).toEqual([]);
+          expect(devices.candidates).toEqual(accounts);
+        });
+      });
+      describe('and the pubkey does not exists on a different network', () => {
+        it('should keep the lists empty', () => {
+          const pubKeys = ['WEBAUTHN-alice'];
+          const tx = getTxFixture({
+            pubKeys,
+            chainId: '4',
+            networkId: 'testnet04',
+          });
+          const accounts = createMockAccounts({
+            chainId: '18',
+            networkId: 'testnet04',
+            pubKeys: ['WEBAUTHN-bob'],
+          });
+          const devices = getAccountsForTx(accounts)(tx);
+          expect(devices.accounts).toEqual([]);
+          expect(devices.candidates).toEqual([]);
+        });
+      });
     });
-    describe('and there is one applicable account', () => {});
-    describe('and there are multiple applicable accounts', () => {
-      describe('and they are all from the same domain', () => {});
-      describe('and they are all from another domain', () => {});
-      describe('and some belong to a different domain', () => {});
+    describe('and there is one applicable account', () => {
+      it('should add the account to the accounts list', () => {
+        const pubKeys = ['WEBAUTHN-alice'];
+        const tx = getTxFixture({
+          pubKeys,
+          chainId: '4',
+          networkId: 'testnet04',
+        });
+        const accounts = createMockAccounts({
+          chainId: '4',
+          networkId: 'testnet04',
+          pubKeys,
+        });
+        const devices = getAccountsForTx(accounts)(tx);
+        expect(devices.accounts).toEqual(accounts);
+        expect(devices.candidates).toEqual([]);
+      });
     });
   });
 });
