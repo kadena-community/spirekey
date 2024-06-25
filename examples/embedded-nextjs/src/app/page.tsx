87d7b770a4576f71b4cc228f695da25cab7963d5
Andy Tang <andy@kadena.io>
2024-06-25 11:52:50 +0200

    Update sign to use transfer with the correct commands and caps

--- examples/embedded-nextjs/src/app/page.tsx
+++ examples/embedded-nextjs/src/app/page.tsx
@@ -1,15 +1,19 @@
 'use client';
 
 import {
+  connect,
   initSpireKey,
   onAccountConnected,
+  sign,
   type Account,
 } from '@kadena-spirekey/sdk';
-import { transfer } from '@kadena/client-utils/coin';
-import { useEffect, useState } from 'react';
-
+import { createTransactionBuilder, Pact, createClient } from '@kadena/client';
+import { FormEvent, useEffect, useState } from 'react';
+const ns = 'n_eef68e581f767dd66c4d4c39ed922be944ede505';
 export default function Home() {
   const [account, setAccount] = useState<Account>();
+  const [receiver, setReceiver] = useState<string>('');
+  const [amount, setAmount] = useState<number>(0);
 
   useEffect(() => {
     initSpireKey({ hostUrl: 'http://localhost:1337' });
@@ -20,45 +24,77 @@ export default function Home() {
   }, []);
 
   const signTransaction = async () => {
-    if (!account) {
-      throw new Error('No account connected');
-    }
+    if (!account) throw new Error('No account connected');
+    if (!receiver) throw new Error('No receiver defined');
+    const tx = createTransactionBuilder()
+      .execution(
+        `(${ns}.webauthn-wallet.transfer
+        "${account.accountName}"
+        "${receiver}"
+      )`,
+      )
+      .setMeta({
+        senderAccount: account.accountName,
+        chainId: account.chainIds[0],
+      });
 
-    const transaction = transfer(
-      {
-        sender: {
-          account: account.accountName,
-          publicKeys: [
-            {
-              pubKey: account.devices[0].guard.keys[0],
-              scheme: 'WebAuthn',
-            },
+    // Note: tx is an instance that is being mutated
+    account.devices.flatMap((d) =>
+      d.guard.keys.map((k) =>
+        tx.addSigner(
+          {
+            pubKey: k,
+            scheme: /^WEBAUTHN-/.test(k) ? 'WebAuthn' : 'ED25519',
+          },
+          (withCap) => [
+            withCap(
+              `${ns}.webauthn-wallet.TRANSFER`,
+              account.accountName,
+              receiver,
+              { decimal: amount.toFixed(8) },
+            ),
+            withCap(
+              `${ns}.webauthn-wallet.GAS_PAYER`,
+              account.accountName,
+              { int: '1' },
+              { decimal: '1' },
+            ),
+            withCap(
+              `${ns}.webauthn-wallet.GAS`,
+              account.accountName,
+            ),
           ],
-        },
-        receiver: 'k:abcd',
-        amount: '1',
-        chainId: '1',
-      },
-      {
-        host: 'http://localhost:8080',
-        defaults: {
-          networkId: 'development',
-        },
-        sign: window.spireKey.sign,
-      },
+        ),
+      ),
     );
-
-    await transaction.execute();
+    tx.setNetworkId('development')
+    sign([tx.createTransaction()])
   };
+
+  const onReceiverChange = (event: FormEvent<HTMLInputElement>) =>
+    setReceiver(event.currentTarget.value);
+  const onAmountChange = (event: FormEvent<HTMLInputElement>) =>
+    setAmount(parseFloat(event.currentTarget.value));
 
   return (
     <main>
-      {!account && (
-        <button onClick={() => window.spireKey.connect()}>Connect</button>
-      )}
+      {!account && <button onClick={() => connect()}>Connect</button>}
       {account && (
         <>
           Connected as {account.alias} ({account.accountName}){' '}
+          <label>
+            receiver:
+            <input onChange={onReceiverChange} value={receiver} />
+          </label>
+          <label>
+            amount:
+            <input
+              onChange={onAmountChange}
+              value={amount}
+              min={0}
+              type="number"
+            />
+          </label>
           <button onClick={signTransaction}>Sign</button>
         </>
       )}
