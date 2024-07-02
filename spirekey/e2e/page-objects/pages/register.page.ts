f6a04e494d01e022c298d1928757946a692a37e8
Ashwin van Dijk <contact@ashwinvandijk.nl>
2024-07-02 21:37:16 +0200

    Partially fix e2e tests

--- spirekey/e2e/page-objects/pages/register.page.ts
+++ spirekey/e2e/page-objects/pages/register.page.ts
@@ -2,12 +2,12 @@ import { Locator, Page } from '@playwright/test';
 
 export class RegisterPage {
   private page: Page;
-  private passKeyBtn: Locator;
+  private continueButton: Locator;
   private alias: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.passKeyBtn = this.page.getByTestId('fingerprint-icon');
+    this.continueButton = this.page.getByRole('button', { name: 'Continue' });
     this.alias = this.page.getByRole('heading', { level: 3 });
   }
 
@@ -25,9 +25,9 @@ export class RegisterPage {
   }
 
   async createPassKey(): Promise<void> {
-    await this.page.getByRole('heading', { name: 'Passkey' }).waitFor();
-    await this.passKeyBtn.click();
-    await this.page.getByTestId('accountNameRevealer').waitFor();
+    await this.page.getByRole('heading', { name: 'Register' }).waitFor();
+    await this.continueButton.click();
+    // await this.page.getByTestId('accountNameRevealer').waitFor();
   }
 
   async getRedirectMessage(message: string): Promise<Locator> {
