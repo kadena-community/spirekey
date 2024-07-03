19f7b57c2b5ccc375aff7a694e46daac3c7d5f2a
Andy Tang <andy@kadena.io>
2024-07-03 14:36:46 +0200

    Style notifications

--- examples/embedded-nextjs/src/app/layout.tsx
+++ examples/embedded-nextjs/src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ backgroundColor: 'antiquewhite' }}>{children}</body>
     </html>
   );
 }
