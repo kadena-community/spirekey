0121d2080a24520da7b932a959716dbf1082438b
Andy Tang <andy@kadena.io>
2024-07-11 12:43:33 +0200

    Add isReady event

--- packages/types/src/events.ts
+++ packages/types/src/events.ts
@@ -2,14 +2,15 @@ import type { SpireKeyAccount } from './account';
 
 export type SpireKeyEvents = {
   connected: SpireKeyAccount;
-  'connected:minted': SpireKeyAccount;
   signed: Record<string, { sig: string; pubKey: string }[]>;
-  'signed:submittable': Record<string, { sig: string; pubKey: string }[]>;
+  isReady: SpireKeyAccount;
+
+  'canceled:connect': void;
+  'canceled:sign': void;
+
   'minimize-notification': void;
   'maximize-notification': void;
   'show-notifications-sidebar': void;
-  'canceled:connect': void;
-  'canceled:sign': void;
 };
 
 export type SpireKeyEventName = keyof SpireKeyEvents;
