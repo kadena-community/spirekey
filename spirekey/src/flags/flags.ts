debeceeae62b634c64b05fd508238bcea5c61537
Andy Tang <andy@kadena.io>
2024-07-23 15:13:57 +0200

    Allow raccounts to be used via a flag in SDK

--- spirekey/src/flags/flags.ts
+++ spirekey/src/flags/flags.ts
@@ -1,1 +1,6 @@
-export const useRAccount = () => process.env.USE_RACCOUNT === 'true';
+export const useRAccount = () => {
+  const params = new URLSearchParams(location.hash || location.search);
+  return (
+    process.env.USE_RACCOUNT === 'true' || params.get('use-raccount') === 'true'
+  );
+};
