279b7b6069cd86df711fbbb51edb47c03d3d1a9a
Andy Tang <andy@kadena.io>
2024-07-31 10:26:26 +0200

    Extract ConnectStep for readability

--- examples/embedded-nextjs/src/app/page.tsx
+++ examples/embedded-nextjs/src/app/page.tsx
@@ -202,6 +202,85 @@ const ExampleStepper = ({ step }: { step: number }) => {
   return <Stepper>{steps}</Stepper>;
 };
 
+const ConnectStep = ({
+  setAccount,
+}: {
+  setAccount: (account: Account) => void;
+}) => {
+  const [wallet, setWallet] = useLocalState(
+    'wallet',
+    'https://spirekey.kadena.io/',
+  );
+  const [networkId, setNetworkId] = useLocalState('networkId', 'testnet04');
+  const [chainId, setChainId] = useLocalState('chainId', '14');
+  const [isLoading, setIsLoading] = useState(false);
+  const onConnect = async () => {
+    try {
+      setIsLoading(true);
+      const account = await connect(networkId, chainId as ChainId);
+      setAccount(account);
+      setAccount(await account.isReady());
+    } catch (e) {
+      console.warn('User canceled', e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <CardContainer>
+      <CardContentBlock
+        visual={<ProductIcon.QuickStart size="xl" />}
+        title="Step 1: Connect your account"
+        description={<ExampleStepper step={0} />}
+      >
+        <Stack flexDirection="column" gap="md">
+          <Select
+            label="Chain"
+            onSelectionChange={(c) => setChainId(c as ChainId)}
+            selectedKey={chainId}
+          >
+            {Array(20)
+              .fill(1)
+              .map((_, i) => (
+                <SelectItem key={i}>{i.toString()}</SelectItem>
+              ))}
+          </Select>
+          <Accordion>
+            <AccordionItem title="Advanced settings">
+              <>
+                <Select
+                  label="Wallet"
+                  onSelectionChange={(w) => setWallet(w as string)}
+                  selectedKey={wallet}
+                >
+                  <SelectItem key="https://spirekey.kadena.io/">
+                    SpireKey
+                  </SelectItem>
+                  <SelectItem key="http://localhost:1337/">Local</SelectItem>
+                </Select>
+                <Select
+                  label="Network"
+                  onSelectionChange={(n) => setNetworkId(n as string)}
+                  selectedKey={networkId}
+                >
+                  <SelectItem key="mainnet01">Mainnet</SelectItem>
+                  <SelectItem key="testnet04">Testnet</SelectItem>
+                  <SelectItem key="development">Devnet</SelectItem>
+                </Select>
+              </>
+            </AccordionItem>
+          </Accordion>
+        </Stack>
+      </CardContentBlock>
+      <CardFooter>
+        <Button isLoading={isLoading} onPress={onConnect}>
+          Connect
+        </Button>
+      </CardFooter>
+    </CardContainer>
+  );
+};
+
 export default function Home() {
   const [account, setAccount] = useState<Account>();
   const [receiver, setReceiver] = useState<string>('');
@@ -266,18 +345,6 @@ export default function Home() {
       setIsLoading(false);
     }
   };
-  const onConnect = async () => {
-    try {
-      setIsLoading(true);
-      const account = await connect(networkId, chainId as ChainId);
-      setAccount(account);
-      setAccount(await account.isReady());
-    } catch (e) {
-      console.warn('User canceled', e);
-    } finally {
-      setIsLoading(false);
-    }
-  };
   return (
     <Stack
       padding="md"
@@ -288,59 +355,7 @@ export default function Home() {
       maxWidth="content.maxWidth"
     >
       {!account && (
-        <CardContainer>
-          <CardContentBlock
-            visual={<ProductIcon.QuickStart size="xl" />}
-            title="Step 1: Connect your account"
-            description={<ExampleStepper step={0} />}
-          >
-            <Stack flexDirection="column" gap="md">
-              <Select
-                label="Chain"
-                onSelectionChange={(c) => setChainId(c as ChainId)}
-                selectedKey={chainId}
-              >
-                {Array(20)
-                  .fill(1)
-                  .map((_, i) => (
-                    <SelectItem key={i}>{i.toString()}</SelectItem>
-                  ))}
-              </Select>
-              <Accordion>
-                <AccordionItem title="Advanced settings">
-                  <>
-                    <Select
-                      label="Wallet"
-                      onSelectionChange={(w) => setWallet(w as string)}
-                      selectedKey={wallet}
-                    >
-                      <SelectItem key="https://spirekey.kadena.io/">
-                        SpireKey
-                      </SelectItem>
-                      <SelectItem key="http://localhost:1337/">
-                        Local
-                      </SelectItem>
-                    </Select>
-                    <Select
-                      label="Network"
-                      onSelectionChange={(n) => setNetworkId(n as string)}
-                      selectedKey={networkId}
-                    >
-                      <SelectItem key="mainnet01">Mainnet</SelectItem>
-                      <SelectItem key="testnet04">Testnet</SelectItem>
-                      <SelectItem key="development">Devnet</SelectItem>
-                    </Select>
-                  </>
-                </AccordionItem>
-              </Accordion>
-            </Stack>
-          </CardContentBlock>
-          <CardFooter>
-            <Button isLoading={isLoading} onPress={onConnect}>
-              Connect
-            </Button>
-          </CardFooter>
-        </CardContainer>
+        <ConnectStep setAccount={setAccount} />
       )}
       {account && (
         <CardContainer>
