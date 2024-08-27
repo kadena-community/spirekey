4ccbf661466a16960c54bc94b4a0143efec5d866
Andy Tang <andy@kadena.io>
2024-08-27 12:23:21 +0200

    Use apollo local resolvers

--- spirekey/src/hooks/useQuery.ts
+++ spirekey/src/hooks/useQuery.ts
@@ -1,19 +1,6 @@
 import { rootValue, schema } from '@/resolvers/local-resolvers';
-import { executeExchange } from '@urql/exchange-execute';
-import {
-  AnyVariables,
-  cacheExchange,
-  Client,
-  DocumentInput,
-  Exchange,
-  ExchangeInput,
-  fetchExchange,
-  makeResult,
-  mapExchange,
-  Operation,
-  useQuery as useUrqlQuery,
-} from 'urql';
-import { filter, map, merge, pipe, tap } from 'wonka';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { Account } from '@kadena/spirekey-types';
 
 const getGraphqlHost = (networkId: string) => {
   if (networkId === 'development') return 'http://localhost:8080/graphql';
@@ -21,55 +8,35 @@ const getGraphqlHost = (networkId: string) => {
     return 'https://graph.testnet.kadena.network/graphql';
   return 'https://graph.kadena.network/graphql';
 };
-export const query = async (
-  networkId: string,
-  query: DocumentInput,
-  variables: AnyVariables,
-) => {
-  const url = getGraphqlHost(networkId);
-  return await urqlClient.query(query, variables, {
-    url,
-  });
-};
-const localExchange = executeExchange({
-  schema,
-  rootValue,
-  context: {
-    query,
+const cache = new InMemoryCache({
+  typePolicies: {},
+});
+const httpLink = new HttpLink({
+  includeUnusedVariables: true,
+  fetch: (_, opts) => {
+    if (!opts?.body) return fetch(getGraphqlHost(''), opts);
+
+    const { variables } = JSON.parse(opts.body.toString());
+    return fetch(getGraphqlHost(variables.networkId), opts);
   },
 });
-const getFieldName = (op: Operation) =>
-  op.query?.definitions?.some((d) =>
-    d?.selectionSet?.selections?.some((s) => s.name?.value === 'accounts'),
-  );
-const customExchange: Exchange =
-  ({ forward, ...rest }) =>
-  (operations$) => {
-    return pipe(
-      operations$,
-      tap((op) => {
-        if (!getFieldName(op)) return localExchange({ forward, ...rest })(op);
-        return forward(op);
-      }),
-    );
-  };
-export const urqlClient = new Client({
-  // the default network to use when not set
-  url: getGraphqlHost('testnet04'),
-  exchanges: [cacheExchange, customExchange],
-});
-
-export const useQuery = (
-  networkId: string,
-  query: DocumentInput,
-  variables: AnyVariables,
-) => {
-  const url = getGraphqlHost(networkId);
-  return useUrqlQuery({
-    query,
-    variables,
-    context: {
-      url,
+export const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+  resolvers: {
+    Query: {
+      accounts(_, { networkId }) {
+        const accString = localStorage.getItem('localAccounts');
+        if (!accString) return [];
+        const accs = JSON.parse(accString);
+        return accs.filter((a: Account) => a.networkId === networkId);
+      },
     },
-  });
-};
+    Mutation: {
+      createPasskey(_, args) {
+        console.warn('DEBUGPRINT[8]: useQuery.ts:24: args=', args);
+        return Promise.resolve('hello');
+      },
+    },
+  },
+});
