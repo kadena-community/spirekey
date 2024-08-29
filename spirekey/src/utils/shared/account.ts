5783ffe57c1212107f1dcf247b5f89edb0fac413
Andy Tang <andy@kadena.io>
2024-08-29 17:46:08 +0200

    no more need to check for old namespace

--- spirekey/src/utils/shared/account.ts
+++ spirekey/src/utils/shared/account.ts
@@ -55,7 +55,7 @@ export const getRAccountFromChain = async ({
   chainId?: ChainId;
 }): Promise<Omit<Account, 'alias'> | null> => {
   const tx = createTransactionBuilder()
-    .execution(`(${namespace}.spirekey.details "${accountName}" coin)`)
+    .execution(`(kadena.spirekey.details "${accountName}" coin)`)
     .setMeta({
       chainId,
     })
