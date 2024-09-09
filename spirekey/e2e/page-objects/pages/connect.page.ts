1c4336cc5c035cbd87131b89ee6d40ceafcd2dce
Andy Tang <andy@kadena.io>
2024-09-09 10:25:15 +0200

    Extract and replace existing connect page

--- spirekey/e2e/page-objects/pages/connect.page.ts
+++ spirekey/e2e/page-objects/pages/connect.page.ts
@@ -1,42 +1,37 @@
+import { WebAuthNHelper } from '@e2e/helpers/webauthn.helper';
 import { Locator, Page } from '@playwright/test';
 
 export class ConnectPage {
-  private page: Page;
-  private registerBtn: Locator;
-  private createBtn: Locator;
-  private createWalletBtn: Locator;
-  private connectBtn: Locator;
-  private completeBtn: Locator;
+  private registerButton: Locator;
+  private recoverButton: Locator;
+  private createWalletButton: Locator;
+  private createAccountButton: Locator;
+  private completeButton: Locator;
 
   constructor(page: Page) {
-    this.page = page;
-    this.registerBtn = this.page.getByRole('button', { name: 'Register' });
-    this.createBtn = this.page.getByRole('button', { name: 'Continue' });
-    this.createWalletBtn = this.page.getByRole('button', { name: 'Create' });
-    this.connectBtn = this.page.getByRole('button', { name: 'Connect' });
-    this.completeBtn = this.page.getByRole('button', { name: 'Complete' });
+    this.registerButton = page.getByRole('button', { name: 'Register' });
+    this.recoverButton = page.getByRole('button', { name: 'Recover' });
+    this.createWalletButton = page.getByRole('button', { name: 'Create' });
+    this.createAccountButton = page.getByRole('button', { name: 'Continue' });
+    this.completeButton = page.getByRole('button', { name: 'Complete' });
+
+    const webauthn = new WebAuthNHelper(page);
+    webauthn.enableWebAuthN();
   }
 
-  async createAccount(): Promise<void> {
-    await this.registerBtn.click();
-    await this.createWalletBtn.click();
-    await this.createBtn.click();
-    await this.completeBtn.waitFor();
-    await this.completeBtn.click();
+  async startRegistration() {
+    await this.registerButton.click();
   }
 
-  async connect(): Promise<void> {
-    await this.connectBtn.waitFor();
-    await this.connectBtn.click();
+  async createNewWallet() {
+    await this.createWalletButton.click();
   }
 
-  async visit(returnUrl: string, networkId: string): Promise<void> {
-    this.page.goto(`/connect?returnUrl=${returnUrl}&networkId=${networkId}`);
+  async createNewAccount() {
+    await this.createAccountButton.click();
   }
 
-  getConnectMessage(returnUrl: string): Locator {
-    return this.page.getByText(
-      `Which account do you want to use to identify on ${returnUrl}?`,
-    );
+  async completeRegistration() {
+    await this.completeButton.click();
   }
 }
--- spirekey/e2e/page-objects/pages/example-connect.page.ts
+++ spirekey/e2e/page-objects/pages/example-connect.page.ts
@@ -1,5 +1,5 @@
-import { WebAuthNHelper } from '@e2e/helpers/webauthn.helper';
 import { Locator, Page } from '@playwright/test';
+import { ConnectPage } from './connect.page';
 
 export class ExampleConnectPage {
   private page: Page;
@@ -43,40 +43,5 @@ export class ExampleConnectPage {
     await this.connectButton.click();
     const popup = await popupPromise;
     return new ConnectPage(popup);
-  }
-}
-
-class ConnectPage {
-  private registerButton: Locator;
-  private recoverButton: Locator;
-  private createWalletButton: Locator;
-  private createAccountButton: Locator;
-  private completeButton: Locator;
-
-  constructor(page: Page) {
-    this.registerButton = page.getByRole('button', { name: 'Register' });
-    this.recoverButton = page.getByRole('button', { name: 'Recover' });
-    this.createWalletButton = page.getByRole('button', { name: 'Create' });
-    this.createAccountButton = page.getByRole('button', { name: 'Continue' });
-    this.completeButton = page.getByRole('button', { name: 'Complete' });
-
-    const webauthn = new WebAuthNHelper(page);
-    webauthn.enableWebAuthN();
-  }
-
-  async startRegistration() {
-    await this.registerButton.click();
-  }
-
-  async createNewWallet() {
-    await this.createWalletButton.click();
-  }
-
-  async createNewAccount() {
-    await this.createAccountButton.click();
-  }
-
-  async completeRegistration() {
-    await this.completeButton.click();
   }
 }
