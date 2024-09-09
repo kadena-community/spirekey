1a19e74956de00e156c64932a880bf38dadeb0fb
Andy Tang <andy@kadena.io>
2024-09-09 15:00:22 +0200

    add e2e folder as input to bust the cache for turbo Fix unused import

--- spirekey/e2e/fixtures/helpers.fixture.ts
+++ spirekey/e2e/fixtures/helpers.fixture.ts
@@ -1,5 +1,4 @@
 import { LocalStorageHelper } from '@e2e/helpers/localStorage.helper';
-import { WebAuthNHelper } from '@e2e/helpers/webauthn.helper';
 import { test as baseTest } from '@playwright/test';
 
 export const test = baseTest.extend<{
