b37b3103a9debc90797013bc4ff36086ec8c9b2a
Steven Straatemans <steven@straatemans.nl>
2024-09-09 11:49:13 +0200

    fixing the style of sdk

--- sdk/src/styles.css.ts
+++ sdk/src/styles.css.ts
@@ -1,14 +1,13 @@
-import { token } from '@kadena/kode-ui/styles';
 import { style } from '@vanilla-extract/css';
 
 export const spirekeyNotification = style({
-  borderRadius: token('radius.lg'),
+  borderRadius: '9px',
   position: 'fixed',
   border: 'none',
   width: '21.5rem',
   height: '3rem',
-  right: token('spacing.md'),
-  top: token('spacing.md'),
+  right: '1rem',
+  top: '1rem',
   zIndex: 999999,
   transition:
     'transform 200ms cubic-bezier(0.33, 1, 0.68, 1), right 200ms cubic-bezier(0.33, 1, 0.68, 1)',
