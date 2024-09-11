d926d7723fe586b00c5c238d3e51393fd506f5cd
Andy Tang <andy@kadena.io>
2024-09-11 18:06:39 +0200

    Add Wallet page

---
+++ spirekey/src/app/(wallet)/accounts/[caccount]/devices/[cid]/wallet/page.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import DeviceCard from '@/components/Card/DeviceCard';
+import { SpireKeyCardContentBlock } from '@/components/SpireKeyCardContentBlock';
+import { useAccounts } from '@/resolvers/accounts';
+import { MonoArrowBack } from '@kadena/kode-icons/system';
+import { Button, maskValue } from '@kadena/kode-ui';
+import {
+  CardContentBlock,
+  CardFixedContainer,
+  CardFooterGroup,
+} from '@kadena/kode-ui/patterns';
+import { atoms } from '@kadena/kode-ui/styles';
+import { useParams, useRouter } from 'next/navigation';
+
+export default function WalletPage() {
+  const params = useParams();
+  const { accounts } = useAccounts();
+  const { push } = useRouter();
+  const caccount = decodeURIComponent(params.caccount.toString());
+  const cid = decodeURIComponent(params.cid.toString());
+  const account = accounts?.find((a) => a.accountName === caccount);
+  const device = account?.devices.find((d) => d['credential-id'] === cid);
+
+  return (
+    <CardFixedContainer>
+      <SpireKeyCardContentBlock
+        title="Wallet"
+        description={`Reveal the mnemonic phrase of your account ${maskValue(caccount)}`}
+        extendedContent={
+          account && device ? (
+            <DeviceCard
+              account={account}
+              device={device}
+              color={device?.color}
+            />
+          ) : null
+        }
+      >
+        <CardFooterGroup>
+          <Button variant="primary">Reveal</Button>
+        </CardFooterGroup>
+      </SpireKeyCardContentBlock>
+      <CardContentBlock
+        title="Mnemonic"
+        description="Make sure to keep your mnemonic phrase safe! If a bad actor get's hold of your mnemonic phrase, they will have full access to your account."
+      ></CardContentBlock>
+      <Button
+        className={atoms({ position: 'absolute', left: 0 })}
+        startVisual={<MonoArrowBack />}
+        style={{ top: -50 }}
+        variant="outlined"
+        onPress={() => push(`/`)}
+      >
+        Back to Accounts
+      </Button>
+    </CardFixedContainer>
+  );
+}
