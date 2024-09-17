a861f43306f49c96476cf3362033ed3e591c414a
Andy Tang <andy@kadena.io>
2024-09-17 15:06:20 +0200

    Add aaguid mapping for displaying icons

--- spirekey/src/utils/getDeviceIcon.tsx
+++ spirekey/src/utils/getDeviceIcon.tsx
@@ -1,9 +1,19 @@
 import { DeviceDesktop } from '@/components/icons/DeviceDesktop';
 import { DevicePhone } from '@/components/icons/DevicePhone';
 import { DeviceSecurityKey } from '@/components/icons/DeviceSecurityKey';
+import Image from 'next/image';
 import { ReactNode } from 'react';
+import aaguidMap from './aaguid.json' with { type: 'json' };
 
+const getAaguid = (map: any, key: string) => {
+  return map[key];
+};
 export const getDeviceIcon = (deviceType: string): ReactNode => {
+  const aaguid = getAaguid(aaguidMap, deviceType);
+  if (aaguid)
+    return (
+      <Image src={aaguid.icon_dark} alt={aaguid.name} width={40} height={40} />
+    );
   switch (deviceType) {
     case 'security-key':
       return <DeviceSecurityKey />;
