a2d731d7c764da10d21f9fe2447a9a9826798eb8
Steven Straatemans <steven@straatemans.nl>
2024-09-18 12:24:41 +0200

    cleanup function

--- spirekey/src/components/OnBoarding/components/OnBoardingStepper/utils.tsx
+++ spirekey/src/components/OnBoarding/components/OnBoardingStepper/utils.tsx
@@ -9,8 +9,6 @@ export const createStatus = (
   step: number | undefined,
   idx: number,
 ): IStepProps['status'] => {
-  if (step === undefined) return 'inactive';
-
   if (step === idx) return 'active';
 
   return 'inactive';
