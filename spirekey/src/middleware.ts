ac89f79478d736da7194f1712ad5db35ae88071b
Steven Straatemans <steven@straatemans.nl>
2024-09-26 09:30:55 +0200

    save pageview event with ONLY user agent info

--- spirekey/src/middleware.ts
+++ spirekey/src/middleware.ts
@@ -1,10 +1,8 @@
-//import { hash } from '@kadena/cryptography-utils';
+import { hash } from '@kadena/cryptography-utils';
 import type { NextRequest } from 'next/server';
+import parser from 'ua-parser-js';
 
-// const createClientId = (request: NextRequest): string => {
-//   const str = `${request.headers.get('user-agent')}${request.headers.get('sec-ch-ua-platform')}`;
-//   return hash(str);
-// };
+const createClientId = (ua: string): string => hash(ua);
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
@@ -12,76 +10,55 @@ export async function middleware(request: NextRequest) {
     throw new Error('google account and api secret are missing');
   }
 
-  //const clientId = createClientId(request);
-
-  const userAgent = request.headers.get('user-agent');
-  console.log(userAgent);
+  const userAgent = request.headers.get('user-agent') ?? '';
+  const ua = parser(userAgent);
   if (userAgent?.includes('Edge Functions')) return;
 
-  console.log(request.url, {
-    userAgent: userAgent,
-    isMobile: request.headers.get('sec-ch-ua-mobile'),
-    platform: request.headers.get('sec-ch-ua-platform'),
+  const clientId = createClientId(userAgent);
+  const data = JSON.stringify({
+    client_id: clientId, // A unique client identifier
+    user_id: clientId, // A unique client identifier
+    events: [
+      {
+        name: 'page_view',
+        params: {
+          page_title: 'Spirekey',
+          page_location: request.url,
+          page_referrer: request.referrer,
+          time_to_section: 123456,
+          send_page_view: true,
+        },
+      },
+      {
+        name: 'page_view_ua',
+        params: {
+          page_title: 'Spirekey',
+          page_location: request.url,
+          page_referrer: request.referrer,
+          time_to_section: 123456,
+          send_page_view: true,
+          browserName: ua.browser.name,
+          browserVersion: ua.browser.version,
+          deviceModel: ua.device.model,
+          deviceType: ua.device.type,
+          deviceVendor: ua.device.vendor,
+          engineName: ua.engine.name,
+          engineVersion: ua.engine.version,
+          osName: ua.os.name,
+          osVersion: ua.os.version,
+          ua: ua.ua,
+        },
+      },
+    ],
   });
 
-  // const options = {
-  //   v: 2,
-  //   // tid: `${process.env.GA_ACCOUNT}`,
-  //   gtm: 'a64e392f914fbb0fb2f065aad16be630',
-  //   _p: 1,
-  //   cid: clientId,
-  //   ul: 'en-gb',
-  //   sr: '1980x1080',
-  //   uaa: 'arm',
-  //   uab: '64',
-  //   uafvl: request.headers.get('user-agent'),
-  //   uamb: request.headers.get('sec-ch-ua-mobile'),
-  //   uam: '',
-  //   uap: request.headers.get('sec-ch-ua-platform'),
-  //   uapv: '14.6.1',
-  //   uaw: 0,
-  //   _s: 0,
-  //   sid: Math.random(),
-  //   sct: 1,
-  //   seg: 1,
-  //   dl: request.url,
-  //   dr: request.headers.get('referrer'),
-  //   dt: 'spirekey',
-  //   en: 'page_view',
-  //   _et: '1000',
-  //   _ss: 1,
-  // };
-
-  // const data = JSON.stringify({
-  //   client_id: clientId, // A unique client identifier
-  //   user_id: clientId, // A unique client identifier
-  //   events: [
-  //     {
-  //       name: 'page_view',
-  //       params: {
-  //         page_title: 'Spirekey',
-  //         page_location: request.url,
-  //         time_to_section: 123456,
-  //         send_page_view: true,
-  //         ...options,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  // const optionsString = Object.entries(options)
-  //   .map(([key, val]) => {
-  //     return `${key}=${val}`;
-  //   })
-  //   .join('&');
-
-  // await fetch(
-  //   `https://www.google-analytics.com/mp/collect?measurement_id=${process.env.GA_ACCOUNT}&api_secret=${process.env.GA_ACCOUNT_API}&${encodeURI(optionsString)}`,
-  //   {
-  //     method: 'POST',
-  //     body: data,
-  //   },
-  // );
+  await fetch(
+    `https://www.google-analytics.com/mp/collect?measurement_id=${process.env.GA_ACCOUNT}&api_secret=${process.env.GA_ACCOUNT_API}`,
+    {
+      method: 'POST',
+      body: data,
+    },
+  );
 }
 
 // See "Matching Paths" below to learn more
