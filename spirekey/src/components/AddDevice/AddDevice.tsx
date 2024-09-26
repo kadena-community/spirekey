80f0b49d870c6d2dff59f2bda247a9b059aa6ca8
Andy Tang <andy@kadena.io>
2024-09-26 10:08:17 +0200

    Use permissions to show what is about to be signed

--- spirekey/src/components/AddDevice/AddDevice.tsx
+++ spirekey/src/components/AddDevice/AddDevice.tsx
@@ -1,6 +1,8 @@
+import { Permissions } from '@/components/Permissions/Permissions';
 import { useAccounts } from '@/resolvers/accounts';
 import { useAddDevice } from '@/resolvers/add-device';
 import { Button, Stack, TextareaField } from '@kadena/kode-ui';
+import { CardContentBlock, CardFooterGroup } from '@kadena/kode-ui/patterns';
 import { useState } from 'react';
 
 export const AddDevice = ({ accountName }: { accountName?: string }) => {
@@ -10,16 +12,48 @@ export const AddDevice = ({ accountName }: { accountName?: string }) => {
   const account = accounts?.find((a) => a.accountName === accountName);
   if (!account) return null;
   return (
-    <Stack flexDirection="column">
-      <Button
-        onPress={async () =>
-          setTxs(await getAddDeviceTxs(account.networkId, account.accountName))
-        }
-        isDisabled={loading || !account}
-      >
-        Add device
-      </Button>
-      <TextareaField value={JSON.stringify(txs, null, 2)} />
-    </Stack>
+    <>
+      <CardContentBlock title="Add Device">
+        <CardFooterGroup>
+          <Button
+            onPress={async () =>
+              setTxs(
+                await getAddDeviceTxs(account.networkId, account.accountName),
+              )
+            }
+            isDisabled={loading || !!txs}
+          >
+            Add device
+          </Button>
+        </CardFooterGroup>
+      </CardContentBlock>
+      {txs && (
+        <CardContentBlock title="Permissions">
+          <Stack flexDirection="column" gap="md">
+            <Permissions
+              module="kadena.spirekey"
+              capabilities={[
+                {
+                  name: 'ADD_DEVICE',
+                  args: [account.accountName],
+                },
+              ]}
+            />
+            <Permissions
+              module="coin"
+              capabilities={[
+                {
+                  name: 'GAS',
+                  args: [],
+                },
+              ]}
+            />
+          </Stack>
+          <CardFooterGroup>
+            <Button>Sign</Button>
+          </CardFooterGroup>
+        </CardContentBlock>
+      )}
+    </>
   );
 };
