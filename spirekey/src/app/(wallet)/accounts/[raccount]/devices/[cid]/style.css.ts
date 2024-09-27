ddab5756635006c88699acce531477a01738c077
Steven Straatemans <steven@straatemans.nl>
2024-09-27 14:05:50 +0200

    add backbutton to the account page on mobile

--- spirekey/src/app/(wallet)/accounts/[raccount]/devices/[cid]/style.css.ts
+++ spirekey/src/app/(wallet)/accounts/[raccount]/devices/[cid]/style.css.ts
@@ -1,6 +1,18 @@
 import { style } from '@kadena/kode-ui';
+import { responsiveStyle } from '@kadena/kode-ui/styles';
 
 export const overviewWrapperClass = style({
   flex: '50%',
   flexWrap: 'wrap',
 });
+
+export const backButtonClass = style([
+  {
+    display: 'block',
+  },
+  responsiveStyle({
+    md: {
+      display: 'none',
+    },
+  }),
+]);
